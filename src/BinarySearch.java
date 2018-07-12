e5a1b2f9a8dc6bb00f5246a0ab63d91c5d246193
shankajarajapakse <sankaja.14@cse.mrt.ac.lk>
2018-07-12 10:58:55 -0700

    Binary search implemented

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,35 @@
-public class BinarySearch {
+import java.util.ArrayList;
+
+public class BinarySearch
+{
+    public static int binSearch(int x, ArrayList<Long> A, int low, int high )
+    {
+        int middle;
+        int mySol;
+
+        if ( low > high )
+        {
+            return( -1 );        // Not found
+        }
+        else
+        {
+            middle = (low + high)/2;
+            if ( x == A.get(middle) )
+            {
+                mySol = middle;         // Found x, return location in array
+            }
+            else if ( x < A.get(middle)  )
+            {
+                mySol = binSearch( x, A, low, middle-1 );  // Solve smaller problem
+            }
+            else  //  x > A[middle]
+            {
+                mySol = binSearch( x, A, middle+1, high );  // Solve smaller problem
+            }
+
+            return ( mySol );
+        }
+    }
+
+
 }
