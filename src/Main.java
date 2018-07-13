422f44b6f62fab1a02375d153f8bde1c8d58f5a3
shankajarajapakse <sankaja.14@cse.mrt.ac.lk>
2018-07-13 01:32:01 -0700

    Randomized binary search implemented Comparison implemented

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
-        DataService ds = new DataService(100000);
+        DataService ds = new DataService(10000000);
         BinarySearch bs = new BinarySearch();
-        System.out.println(bs.BinSearch(1032,ds.getArray(),0,ds.getArray().size()));
+        RandomizedBS rbs = new RandomizedBS();
+
+        Long startTime = System.nanoTime();
+        int y;
+
+        for (long n : ds.getSearchList())
+        {
+            y = bs.binSearch((int) n,ds.getArray(),0,ds.getArray().size());
+        }
+
+        Long endBS = System.nanoTime();
+        System.out.println("Time taken to binary search            = " + (endBS-startTime) );
+        Long startRBS = System.nanoTime();
+
+        for (long n : ds.getSearchList())
+        {
+            y = rbs.randomizedBinSearch((int) n,ds.getArray(),0,ds.getArray().size());
+        }
+
+        System.out.println("Time taken to randomized binary search = " + (System.nanoTime()-startRBS) );
     }
 }
--- src/RandomizedBS.java
+++ src/RandomizedBS.java
@@ -1,2 +1,35 @@
-public class RandomizedBS {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomizedBS
+{
+    public static int randomizedBinSearch(int x, ArrayList<Long> A, int low, int high )
+    {
+        int middle;
+        int sol;
+
+        if ( low > high )
+        {
+            return( -1 );        // Not found
+        }
+        else
+        {
+            Random rand = new Random();
+            middle = rand.nextInt( high - low + 1 )+low ;
+            if ( x == A.get(middle) )
+            {
+                sol = middle;         // Found x, return location in array
+            }
+            else if ( x < A.get(middle)  )
+            {
+                sol = randomizedBinSearch( x, A, low, middle-1 );  // Solve smaller problem
+            }
+            else  //  x > A[middle]
+            {
+                sol = randomizedBinSearch( x, A, middle+1, high );  // Solve smaller problem
+            }
+
+            return ( sol );
+        }
+    }
 }
